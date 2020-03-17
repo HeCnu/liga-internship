2d5117fa327b338bc9dc5e1eb4170faa37814d38
Damir <damir400@mail.ru>
2020-03-17 21:39:02 +0300

    рефакторинг кода

--- javacore-song-task/src/main/java/ru/liga/songtask/util/AnalyzeMidi.java
+++ javacore-song-task/src/main/java/ru/liga/songtask/util/AnalyzeMidi.java
@@ -1,7 +1,6 @@
 package ru.liga.songtask.util;
 
 import com.leff.midi.MidiFile;
-import com.leff.midi.MidiTrack;
 import com.leff.midi.event.MidiEvent;
 import com.leff.midi.event.meta.Lyrics;
 import com.leff.midi.event.meta.Tempo;
@@ -14,36 +13,77 @@ import ru.liga.songtask.domain.Note;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class AnalyzeMidi {
 
     private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void analizeMidiFile(MidiFile midiFile, Tempo last) {
-//        List<Note> notesAll = new ArrayList<>();
-//        for (int numOfTracks = 1; numOfTracks < midiFile.getTracks().size(); numOfTracks++) {
-//            List<Note> notes =  SongUtils.eventsToNotes(midiFile.getTracks().get(numOfTracks).getEvents());
-//            List<MidiEvent> midiEvents = midiFile.getTracks().get(numOfTracks).getEvents().stream()
-//                    .filter(midiEvent -> midiEvent.toString().contains("Text:")).collect(Collectors.toList());
-//            if (!notes.isEmpty()){
-//                notesAll.addAll(notes);
-//            }
-//        }
-//        List<Note> uniqueNotes = searchingUniquePlacesForVocal(last, midiFile, notesAll);
-
         List<Note> uniqueNotes = processOfGettingUniqueNotesList(midiFile);
-
         searchRangeOfNotes(uniqueNotes);
         countOfNotesSameDuration(last, midiFile, uniqueNotes);
         countOfSameNotes(last, midiFile, uniqueNotes);
     }
 
     private static List<Note> processOfGettingUniqueNotesList(MidiFile midiFile) {
-        List<Note> uniqueNotes = new ArrayList<>();
-        List<Note> allNotes = new ArrayList<>();
-        List<Vocal> finalVocalEventsList = new ArrayList<>();
+        List<Vocal> finalVocalEventsList = getVocalsList(midiFile);
+        logger.debug("List vocal events was created");
+        List<List<Note>> etnChangedLists = getETNChangedLists(midiFile);
+        logger.debug("List with all of tracks was created");
+        int sizeTrackFinal = getSizeTrackFinal(finalVocalEventsList, etnChangedLists);
+        logger.debug("Some size of required track was defined");
+        List<Note> finalListNote = new ArrayList<>();
+        for (List<Note> list: etnChangedLists) {
+            if (list.size() == sizeTrackFinal)
+                finalListNote = list;
+        }
 
+        return finalListNote;
+    }
+
+    private static int getSizeTrackFinal(List<Vocal> finalVocalEventsList, List<List<Note>> etnChangedLists) {
+        List<Integer> trackSizesList = getTrackSizesList(etnChangedLists);
+        logger.debug("Array with size tracks was created");
+
+        int sizeTrack = finalVocalEventsList.size(),
+            sizeTrackMin = 0,
+            sizeTrackMax = 0;
+        for (int i = 0; i < trackSizesList.size(); i++) {
+            if(trackSizesList.get(i) <= sizeTrack)
+                sizeTrackMin = trackSizesList.get(i);
+        }
+        logger.debug("Min size of reqiured track - ", sizeTrackMin);
+        for (int i = trackSizesList.size() - 1; i >= 0; i--) {
+            if(trackSizesList.get(i) >= sizeTrack)
+                sizeTrackMax = trackSizesList.get(i);
+        }
+        logger.debug("Min size of reqiured track - ", sizeTrackMin);
+
+        if (Math.abs(sizeTrackMax - trackSizesList.size()) >= Math.abs(sizeTrackMin - trackSizesList.size()))
+            return sizeTrackMin;
+        else
+            return sizeTrackMax;
+    }
+
+    private static List<Integer> getTrackSizesList(List<List<Note>> etnChangedLists) {
+        List<Integer> trackSizesList = etnChangedLists.stream()
+                .map(x -> x.size())
+                .collect(Collectors.toList());
+
+        Collections.sort(trackSizesList);
+        return trackSizesList;
+    }
+
+    private static List<List<Note>> getETNChangedLists(MidiFile midiFile) {
+        List<List<Note>> eventsToNoteChangedList = new ArrayList<>();
+        midiFile.getTracks().stream()
+                .forEachOrdered(midiTrack ->  eventsToNoteChangedList.add(SongUtils.eventsToNotes(midiTrack.getEvents())));
+        return eventsToNoteChangedList;
+    }
+
+    private static List<Vocal> getVocalsList(MidiFile midiFile) {
+
+        List<Vocal> finalVocalEventsList = new ArrayList<>();
 
         for (int numOfTracks = 1; numOfTracks < midiFile.getTracks().size(); numOfTracks++) {
             List<Vocal> tempVocalEventsList = findVocalEventsFromTrack(midiFile.getTracks().get(numOfTracks).getEvents());
@@ -51,70 +91,7 @@ public class AnalyzeMidi {
                 finalVocalEventsList.addAll(tempVocalEventsList);
             }
         }
-
-        List<List<Note>> eventsToNoteChangedList = new ArrayList<>();
-        midiFile.getTracks().stream()
-                .forEachOrdered(midiTrack ->  eventsToNoteChangedList.add(SongUtils.eventsToNotes(midiTrack.getEvents())));
-
-        List<Integer> trackSizesList = eventsToNoteChangedList.stream()
-                .map(x -> x.size())
-                .collect(Collectors.toList());
-
-        Collections.sort(trackSizesList);
-
-        int sizeTrack = finalVocalEventsList.size();
-        int sizeTrackMin = 0;
-        int sizeTrackMax = 0;
-        int sizeTrackFinal = 0;
-        for (int i = 0; i < trackSizesList.size(); i++) {
-            if(trackSizesList.get(i) <= sizeTrack)
-                sizeTrackMin = trackSizesList.get(i);
-        }
-        for (int i = trackSizesList.size() - 1; i >= 0; i--) {
-            if(trackSizesList.get(i) >= sizeTrack)
-                sizeTrackMax = trackSizesList.get(i);
-        }
-
-        if (Math.abs(sizeTrackMax - trackSizesList.size()) >= Math.abs(sizeTrackMin - trackSizesList.size()))
-            sizeTrackFinal = sizeTrackMin;
-        else
-            sizeTrackFinal = sizeTrackMax;
-
-        List<Note> finalListNote = new ArrayList<>();
-        for (List<Note> list: eventsToNoteChangedList) {
-            if (list.size() == sizeTrackFinal)
-                finalListNote = list;
-        }
-
-//        for (int numOfTracks = 1; numOfTracks < midiFile.getTracks().size(); numOfTracks++) {
-//            List<Vocal> tempVocalEventsList = findVocalEventsFromTrack(midiFile.getTracks().get(numOfTracks).getEvents());
-//            List<Note> instrumentEventsList = SongUtils.eventsToNotes(midiFile.getTracks().get(numOfTracks).getEvents());
-//            if (!tempVocalEventsList.isEmpty()){
-//                finalVocalEventsList.addAll(tempVocalEventsList);
-//            }
-//            if (!instrumentEventsList.isEmpty()){
-//                allNotes.addAll(instrumentEventsList);
-//            }
-//        }
-
-//        finalVocalEventsList.stream()
-//                .forEachOrdered( vocalEvent ->
-//                        uniqueNotes.addAll(
-//                            (List) allNotes.stream()
-//                            .filter(note -> (note.startTick() >= vocalEvent.getStartTick() && note.endTickInclusive() <= vocalEvent.getEndTick()))
-//                            .collect(Collectors.toList())
-//                        )
-//                );
-
-        for (Vocal vocalEvent: finalVocalEventsList) {
-            for (Note note: allNotes) {
-                if (note.startTick() >= vocalEvent.getStartTick() && note.startTick() <= vocalEvent.getEndTick()) {
-                    uniqueNotes.add(note);
-                }
-            }
-        }
-
-        return finalListNote;
+        return finalVocalEventsList;
     }
 
     private static List<Vocal> findVocalEventsFromTrack(TreeSet<MidiEvent> midiEvents) {
@@ -122,31 +99,15 @@ public class AnalyzeMidi {
 
         for (MidiEvent event: midiEvents) {
             if ((event instanceof Lyrics || event instanceof Text) && event.getTick() > 0) {
+                logger.debug("Track with instance Lyrics or Text was found. Event body: \n{}\n", event.toString());
                 long endTick = 0L;
                 if (event.getTick() == event.getDelta()) {
                     endTick = event.getTick() + 120L;
                 } else {
                     endTick = event.getTick() + event.getDelta();
                 }
-
                 Vocal vocal = new Vocal(event.toString(), event.getTick(), endTick);
                 resultList.add(vocal);
-            }
-        }
-
-        return resultList;
-    }
-
-    private static List<Note> findNotesLyricsEventsFromTrack1(MidiTrack midiTrack) {
-        List<Note> resultList = new ArrayList<>();
-        List<Note> notes =  SongUtils.eventsToNotes(midiTrack.getEvents());
-
-        for (MidiEvent event: midiTrack.getEvents()) {
-            if ((event instanceof Lyrics || event instanceof Text) && event.getTick() > 0) {
-                List<Note> findedLyricsNoteList = notes.stream()
-                        .filter(note -> (note.startTick() >= event.getTick() && note.endTickInclusive() <= (event.getTick() + event.getDelta())))
-                        .collect(Collectors.toList());
-                resultList.addAll(findedLyricsNoteList);
             }
         }
 
@@ -216,69 +177,4 @@ public class AnalyzeMidi {
         return uniqueNotesList.stream()
                 .filter(uniqueNote -> enteriesNote.equals(uniqueNote.sign().fullName())).count();
     }
-
-    private static List<Note> searchingUniquePlacesForVocal(Tempo last, MidiFile midiFile, List<Note> notesAll) {
-
-        logger.debug("Start function of searching places for vocal");
-        Long startPlayRequiredNote, finishPlayRequiredNote, startPlayNote;
-        List<Note> foundNotes = new ArrayList<>();
-        for (int i = 0; i < notesAll.size(); i++) {
-            startPlayRequiredNote = notesAll.get(i).startTick();
-            finishPlayRequiredNote = notesAll.get(i).endTickInclusive();
-            int counterRepeatedNotes = 0;
-            for (int j = 0; j < notesAll.size(); j++) {
-                startPlayNote = notesAll.get(j).startTick();
-                if (startPlayRequiredNote <= startPlayNote && startPlayNote < finishPlayRequiredNote) {
-                    counterRepeatedNotes ++;
-                    if (counterRepeatedNotes > 1) {
-                        break;
-                    }
-                }
-            }
-            if (counterRepeatedNotes == 1) {
-                logger.debug("[" + i + "] Нота " + notesAll.get(i).sign().fullName() + " с длительностью " + SongUtils.tickToMs(last.getBpm(), midiFile.getResolution(), notesAll.get(i).durationTicks()) + "мс");
-                foundNotes.add(notesAll.get(i));
-            }
-        }
-
-        logger.debug("Finish function of searching places for vocal. Function returned {} elements in list", foundNotes.size());
-        return foundNotes;
-    }
-
-//    private static List<Note> searchingUniquePlacesForVocal(Tempo last, MidiFile midiFile, List<Note> notesAll) {
-//
-//        logger.debug("Start function of searching places for vocal");
-//        Long startPlayRequiredNote, finishPlayRequiredNote, startPlayNote;
-//        List<Note> foundNotes = new ArrayList<>();
-//
-//        for (Note note: notesAll) {
-//            long lastCheckedVariable = 0;
-//            if (note.startTick() > lastCheckedVariable) {
-//
-//            }
-//        }
-//
-//        for (int i = 0; i < notesAll.size(); i++) {
-//            startPlayRequiredNote = notesAll.get(i).startTick();
-//            finishPlayRequiredNote = notesAll.get(i).endTickInclusive();
-//            int counterRepeatedNotes = 0;
-//            for (int j = 0; j < notesAll.size(); j++) {
-//                startPlayNote = notesAll.get(j).startTick();
-//                if (startPlayRequiredNote <= startPlayNote && startPlayNote < finishPlayRequiredNote) {
-//                    counterRepeatedNotes ++;
-//                    if (counterRepeatedNotes > 1) {
-//                        break;
-//                    }
-//                }
-//            }
-//            if (counterRepeatedNotes == 1) {
-//                logger.debug("[" + i + "] Нота " + notesAll.get(i).sign().fullName() + " с длительностью " + SongUtils.tickToMs(last.getBpm(), midiFile.getResolution(), notesAll.get(i).durationTicks()) + "мс");
-//                foundNotes.add(notesAll.get(i));
-//            }
-//        }
-//
-//        logger.debug("Finish function of searching places for vocal. Function returned {} elements in list", foundNotes.size());
-//        return foundNotes;
-//    }
-
 }
